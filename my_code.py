93938e8959755af95b30f648c3695919983d6df2
parkerschwantes <23pschwantes@athenian.org>
2020-11-10 12:09:59 -0800

    update- all done

--- my_code.py
+++ my_code.py
@@ -1,5 +1,5 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
-#  
+# Megan, Adrian, Milan
 sum_list = 0
 
 def avg_temp(sum_list):
@@ -10,9 +10,9 @@ def avg_temp(sum_list):
             list_length = int(list_length)
         for i in range(1, list_length):
             sum_list = sum_list + int(line_list[i])
-        final_sum = sum_list/list_length
-        round(final_sum, 2)
-    return final_sum
+        final_sum = sum_list/(list_length - 1)
+        final_rounded = round(final_sum, 2)
+    return final_rounded
 
 
 if __name__ == '__main__':
